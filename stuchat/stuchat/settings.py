2eda00d25b41f1875c2570f18d2757329ad013fb
RishabhTyagii <rishabtyagi55@gmail.com>
2026-01-25 19:14:14 +0530

    backend prod

--- stuchat/stuchat/settings.py
+++ stuchat/stuchat/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%jur&$8p(vny++shc*xe9f6xs53x@bh)r$g)ek5wu2bynxztv!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
