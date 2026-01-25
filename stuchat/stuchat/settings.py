2cc1524760e453134e6d7e2b872933f7c07ac9a7
RishabhTyagii <rishabtyagi55@gmail.com>
2026-01-26 00:30:17 +0530

    AWS Aroura RDS

--- stuchat/stuchat/settings.py
+++ stuchat/stuchat/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import pymysql
+pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 from datetime import timedelta
@@ -114,8 +116,15 @@ WSGI_APPLICATION = 'stuchat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database-1',
+        'USER': 'admin',
+        'PASSWORD': 'Tarun123',
+        'HOST': 'your-rds-endpoint.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
