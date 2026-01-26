c3ddfea3e5d6f0437badfddd48531740b19ebf45
RishabhTyagii <150930058+RishabhTyagii@users.noreply.github.com>
2026-01-26 21:19:53 +0530

    Refactor ASGI application setup for clarity

--- stuchat/stuchat/asgi.py
+++ stuchat/stuchat/asgi.py
@@ -1,13 +1,18 @@
 import os
 from django.core.asgi import get_asgi_application
+
+# 🔥 VERY IMPORTANT: sabse pehle settings load
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stuchat.settings")
+
+# 🔥 Django setup complete yahin hota hai
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stuchat.settings")
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
     ),
