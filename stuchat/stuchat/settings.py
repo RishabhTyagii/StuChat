9f4903c0b5c149c469e9eca4727be9ca65cbf25e
RishabhTyagii <150930058+RishabhTyagii@users.noreply.github.com>
2026-01-26 21:07:40 +0530

    Switch CHANNEL_LAYERS to use Redis backend

--- stuchat/stuchat/settings.py
+++ stuchat/stuchat/settings.py
@@ -64,20 +64,20 @@ AUTH_USER_MODEL = 'users.User'
 ROOT_URLCONF = 'stuchat.urls'
 ASGI_APPLICATION = 'stuchat.asgi.application'
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     }
+# }
 
 
 MIDDLEWARE = [
