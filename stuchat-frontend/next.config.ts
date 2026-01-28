f7f4b6cdfb8e42a48a60d532368be3346785c4ea
RishabhTyagii <150930058+RishabhTyagii@users.noreply.github.com>
2026-01-28 12:37:51 +0530

    Add remote image pattern for S3 bucket

--- stuchat-frontend/next.config.ts
+++ stuchat-frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yourbaazartest.s3.ap-south-1.amazonaws.com",
+        pathname: "/media/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
